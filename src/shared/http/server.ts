db835ad54f31c27f497bd5f428ba674c2bdb2b86
Michael Gomes Ferreira <michael.21.2012@hotmail.com>
2022-02-18 20:06:27 -0300

    Configuração inicial do express

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,19 @@
-console.log('Hello Dev!')
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+/* Inicializa o express */
+const app = express();
+
+/* Não aceita requisição de qualquer endereço */
+app.use(cors());
+
+/* Habilita retornos em JSON */
+app.use(express.json());
+
+/* Usa as rotas do arquivo de rotas */
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('Server started on pot 3333!');
+});
