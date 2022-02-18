a92e49ddd4fbcff949950a83148c027e49456144
Michael Gomes Ferreira <michael.21.2012@hotmail.com>
2022-02-18 20:23:45 -0300

    Adiciona middleware para tratamento de erros na aplicação

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { NextFunction, Request, response, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 /* Inicializa o express */
 const app = express();
@@ -13,6 +14,20 @@ app.use(express.json());
 
 /* Usa as rotas do arquivo de rotas */
 app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('Server started on pot 3333!');
